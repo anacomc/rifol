e666e33143a04325bd97d89ffd85b663289cc1c7
anacomc <46031201+anacomc@users.noreply.github.com>
2026-07-30 01:22:36 -0400

    Update newserver.js

--- newserver.js
+++ newserver.js
@@ -68,7 +68,8 @@ app.get('/validar-clave', async (req, res) => {
             // Enviamos la actualización del contador de vuelta a Supabase
             // const urlUpdate = "https://qajwpjecppwvlfbuhhey.supabase.co." + encodeURIComponent(clave);
             // const urlBase   = "https://qajwpjecppwvlfbuhhey.supabase.co/rest/v1/licencias?clave=eq.";
-            const urlUpdate = "https://qajwpjecppwvlfbuhhey.supabase.co/rest/v1/licencias?clave=eq." + encodeURIComponent(clave) ;
+            // const urlUpdate = "https://qajwpjecppwvlfbuhhey.supabase.co/rest/v1/licencias?clave=eq." + encodeURIComponent(clave) ;
+            const urlUpdate = urlBase + encodeURIComponent(clave) ;
             await fetch(urlUpdate, {
                 method: 'PATCH',
                 headers: {
