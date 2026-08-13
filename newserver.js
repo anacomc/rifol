8817fab0b35eafe25c7701aef7ab14186f7284a3
anacomc <46031201+anacomc@users.noreply.github.com>
2026-08-13 19:43:33 -0400

    Update newserver.js

--- newserver.js
+++ newserver.js
@@ -490,5 +490,5 @@ app.post('/activar-licencia-online', async (req, res) => {
 //**************************************************************************************************************************************
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`newserver.js Servidor corriendo en el puerto ${PORT}`);
+    console.log(`newserver.js 1. Servidor corriendo en el puerto ${PORT}`);
 });
