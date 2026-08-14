02ec87e29d8df9b0c25adc34a9bdfe28384dfc8d
anacomc <46031201+anacomc@users.noreply.github.com>
2026-08-14 18:43:28 -0400

    Update log message for server startup

--- newserver.js
+++ newserver.js
@@ -632,5 +632,5 @@ app.post('/validar-acceso-diario', async (req, res) => {
 //**************************************************************************************************************************************
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`newserver.js 3. Servidor corriendo en el puerto ${PORT}`);
+    console.log(`newserver.js 4. Servidor corriendo en el puerto ${PORT}`);
 });
